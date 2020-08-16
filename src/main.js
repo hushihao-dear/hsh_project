ab37f28ebf36c7e7e79bba9ad3d040663162cd0e
hsh-dear <1145724236@qq,com>
2020-08-16 11:07:20 +0800

    333

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
 import Resource from 'vue-resource'
 import store from './store'
 Vue.use(Resource)
-Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 
